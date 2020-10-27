9c2f4ab467044d6936328e5648cc0ade633a9915
Erin Corcoran <corcorec@bc.edu>
2020-10-26 20:37:01 -0400

    total calculated

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 let unemp = d3.csv('unemployment.csv', d3.autoType).then( d => {
     console.log(d)
-    console.log(d3.sum(d))
+    total_count = 0
+    d.forEach(function(d) {
+        total_count = 0
+        total_count += d.Agriculture
+        total_count += d['Wholesale and Retail Trade']
+        total_count += d.Manufacturing
+        total_count += d['Leisure and hospitality']
+        total_count += d['Business services']
+        total_count += d.Construction
+        total_count += d['Education and Health']
+        total_count += d.Government
+        total_count += d.Finance
+        total_count += d['Self-employed']
+        total_count += d.Other
+        total_count += d['Transportation and Utilities']
+        total_count += d.Information
+        total_count += d['Mining and Extraction']
+        d['total'] = total_count // add total count to dataset
+    }) 
+    
 });
